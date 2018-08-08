e1d704f79c45435996419e52670f6ce9a478263b
loanpasionek <l.pasionek@gmail.com>
2018-08-08 13:45:23 +0200

    ajout de la page split, responsive OK

--- split-page.php
+++ split-page.php
@@ -1,16 +1,30 @@
 <?php /* Template Name: Split Page */ ?>
 
 <?php get_header(); ?>
-<section class="h100vh">
+
+<section class="h100vh block-on-med-and-down">
   <div class="split left">
     <div class="centered">
-      <h2 class="rotated playfair color1 fs60">BOURGOGNE</h2>
+      <h2 class="rotated playfair color1 h2-sm">
+        <a href="http://www.kelles-energies-bourgogne.fr/" class="nodeco color1">
+        <span class="fs40 fs90-sm">K'ELLES ENERGIES</span>
+
+          <br>
+          <span class="fs60 fs160-sm spacing20">BOURGOGNE</span>
+        </a>          
+      </h2>
     </div>
   </div>
 
-  <div class="split right">
+  <div class="split right marginSplit">
     <div class="centered">
-      <h2 class="rotated playfair fs60"><a href="http://www.kelles-energies-franche-comte.fr/" class="nodeco color1">FRANCHE-COMTE</a></h2>
+      <h2 class="rotated playfair color1 h2-sm">
+        <a href="http://www.kelles-energies-franche-comte.fr/" class="nodeco color1">
+          <span class="fs40 fs90-sm">K'ELLES ENERGIES</span>
+            <br>
+          <span class="fs60 fs160-sm">FRANCHE-COMTE</span>
+        </a>
+      </h2>
     </div>
   </div>
 </section>
